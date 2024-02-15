bba5dbdce534254b0b237a31b7908022151ab14d
Tonin <73282343+Ameno1215@users.noreply.github.com>
2024-02-15 08:10:04 +0100

    Update page.tsx

--- src/app/calendrier/page.tsx
+++ src/app/calendrier/page.tsx
@@ -1,11 +1,11 @@
-import styles from "../styles/calendrier.css"
+
 
 
 export default function Calendrier() {
     return (
 
       
-        <h1 className={`text-4xl ${styles.animateFadeInUp}`}>Votre texte d'accueil</h1>
+        <h1 className="text-4xl">Votre texte d'accueil</h1>
 
     )
   }
